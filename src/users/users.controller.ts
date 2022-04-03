723609cd4a6b574d6aa7888bfa474258ffb71265
Kacj Szlorc <kacjszlorc@Kacjs-MacBook-Pro.local>
2022-04-03 21:00:15 +0200

    feat(): init

---
+++ src/users/users.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/user.dto';
+import { User } from './interfaces/user.interface';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  async findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+}
